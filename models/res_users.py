9ca50ca4f00be8c18f3478c825db826de38e6e06
Your Name <you@example.com>
2021-10-17 17:17:39 +0330

    ff

--- models/res_users.py
+++ models/res_users.py
@@ -7,6 +7,8 @@ from odoo import fields, models
 class Partner(models.Model):
     _inherit = 'res.partner'
 
+    dd=fields.Char("dd")
+    
     _sql_constraints = [
         ('cne_unique', 'unique(mobile)', 'mobile number already exists!')
     ]
