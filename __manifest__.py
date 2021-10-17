c11b1ab16a4098ec24980291e8de5597666147a4
Your Name <you@example.com>
2021-10-17 16:45:13 +0330

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+{
+    'name': 'onlyonenumber',
+
+    'version': '1.1',
+
+    'author': 'AG from the WORLD',
+
+    'summary': "Customer Club System",
+
+    'sequence': 1,
+
+    'description': "only one mobile number for any contacts"
+    "Odoo v14",
+
+    'category': 'Administration',
+
+    'website': 'https://freeweblearns.blogspot.com',
+
+    'depends': ['base'],
+
+    'data': [
+      
+    ],
+
+    'demo': []
+
+}
---
+++ models/res_users.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class Partner(models.Model):
+    _inherit = 'res.partner'
+
+    _sql_constraints = [
+        ('cne_unique', 'unique(mobile)', 'mobile number already exists!')
+    ]
+
+
